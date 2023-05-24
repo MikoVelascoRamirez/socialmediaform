8e11c9d705062eb97db54118c1729fc7f93dae4a
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-05-24 13:54:26 -0600

    2. feat: creating header content.

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<header>
+    <nav>
+        <a href="">El blog del Maik</a>
+        <ul>
+            <li>Home</li>
+            <li>About me</li>
+            <li>Preferences</li>
+            <li>Store</li>
+        </ul>
+        <section>
+            <a href="#"><button>SIGN UP</button></a>
+            <a href="#"><button>LOGIN</button></a>
+        </section>
+    </nav>
+</header>
