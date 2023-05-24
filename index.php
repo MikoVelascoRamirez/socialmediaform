4a7f03a8ff9d6478be0927529c1244375060598b
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-05-24 14:41:26 -0600

    4. feat: creating sign up form

--- index.php
+++ index.php
@@ -11,6 +11,20 @@
     <title>Login System</title>
 </head>
 <body>
-    
+    <main>
+        <section>
+            <h2>SIGN UP</h2>
+            <form action="" method="post">
+                <small>Don't have an account yet? Sign up here</small>
+                <fieldset>
+                    <input type="text" name="username" placeholder="Username">
+                    <input type="password" name="signup_password" placeholder="Password">
+                    <input type="password" name="signup_rep_password" placeholder="Repeat Password">
+                    <input type="email" name="email" placeholder="E-mail">
+                    <input type="submit" value="SIGN UP" name="inp_signup">
+                </fieldset>
+            </form>
+        </section>
+    </main>
 </body>
 </html>
