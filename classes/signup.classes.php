ac05a3bef1d606350d7d16dfb3d209064447b698
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-05-29 15:40:36 -0600

    21. feat: insert a new user to database through a setUser().

--- classes/signup.classes.php
+++ classes/signup.classes.php
@@ -10,4 +10,21 @@ class SignUp extends Dbh{
 
         return $stmt->rowCount();
     }
+
+    protected function setUser($uid, $pwd, $email){
+        $sql = "INSERT INTO users (users_uid, users_pwd, users_email) VALUES (?, ?, ?)";
+
+        $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+        $stmt = $this->connect()->prepare($sql);
+        $result = $stmt->execute([$uid, $hashedPwd, $email]);
+
+        if(!$result){
+            $stmt = null;
+            header("Location: ../index.php?error=stmtfailed");
+            exit();
+        }
+
+        $stmt = null;
+    }
 }
