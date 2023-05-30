7ca87ac779f476e4ee7fac73fe1e3893cd703925
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-05-29 20:05:06 -0600

    23. validating empty inputs and implementing into loginUser handler

--- classes/login-contr.classes.php
+++ classes/login-contr.classes.php
@@ -9,4 +9,24 @@ class LoginContr extends Login{
         $this->uid = $uid;
         $this->pass = $pass;
     }
+
+    public function loginUser(){
+        if(!$this->emptyInput()) {
+            echo "Empty Input!";
+            header("Location: ../index.php?error=emptyinput");
+            exit();
+        }
+    }
+
+    private function emptyInput(){
+        $object_props = get_object_vars($this);
+        // var_dump($object_props = get_object_vars($this));
+        foreach($object_props as $key => $value){
+            // echo $object_props[$key] . $object_props[$value];
+            if (empty($object_props[$key])){
+                return false;
+            }
+        }
+        return true;
+    }
 }
