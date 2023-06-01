9524643cd69b583fd448cc4f326076b7776a5e78
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-06-01 15:47:55 -0600

    39. setting default user's information profile.

--- UserProfile/classes/profileinfo-contr.classes.php
+++ UserProfile/classes/profileinfo-contr.classes.php
@@ -10,4 +10,17 @@ class ProfileInfoContr extends ProfileInfo{
         $this->userUid = $userUid;
     }
 
+    public function setDefaultProfileInfo(){
+        $profileAbout = "Tell people about yourself!";
+        $profileTitle = "Hi, I'm {$this->userUid}";
+        $profileText = "Welcome to my profile page!";
+
+
+        $this->setProfileInfo($profileAbout, $profileTitle, $profileText, $this->userId);
+    }
+
+    private function setProfileInfo($profileAbout, $profileTitle, $profileText, $userId){
+        $this->setNewProfile($profileAbout, $profileTitle, $profileText, $userId);
+    }
+
 }
