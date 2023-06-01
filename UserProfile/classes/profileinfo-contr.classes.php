d136bf605ce922702c7884f4d5e53aa5b41cdba6
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-06-01 15:46:10 -0600

    38. Creating basic class structure of ProfileInfo Controller.

---
+++ UserProfile/classes/profileinfo-contr.classes.php
@@ -0,0 +1,13 @@
+<?php
+
+class ProfileInfoContr extends ProfileInfo{
+
+    private $userId;
+    private $userUid;
+
+    public function __construct($userId, $userUid){
+        $this->userId = $userId;
+        $this->userUid = $userUid;
+    }
+
+}
