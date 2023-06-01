4c291c4406fb493078c11be2dfbbfda15ef5e797
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-06-01 15:40:58 -0600

    37. creating a new profile.

--- UserProfile/classes/profileinfo.classes.php
+++ UserProfile/classes/profileinfo.classes.php
@@ -13,4 +13,13 @@ class ProfileInfo extends Dbh {
             exit();
         }
     }
+
+    protected function setNewProfile($profileAbout, $profileTitle, $profileText, $profileId){
+        $sql = "INSERT INTO profiles (profiles_about, profiles_introtitle, profiles_introtext, users_id) VALUES (?, ?, ?, ?)";
+        $stmt = $this->connect()->prepare($sql);
+        $result = $stmt->execute([$profileAbout, $profileTitle, $profileText, $profileId]);
+
+
+        print_r($result);
+    }
 }
