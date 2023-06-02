13ec7aac4f6813dd16cd6bf7485bb13d92ed620b
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-06-02 16:04:28 -0600

    49. getting profile data at instance's class.

--- UserProfile/classes/profileinfo-view.classes.php
+++ UserProfile/classes/profileinfo-view.classes.php
@@ -9,5 +9,20 @@ class ProfileInfoView extends ProfileInfo{
  
     public function __construct($id){
         $this->id = $id;
+
+        $info = $this->getProfile($this->id);
+
+        $this->about = $info['profiles_about'];
+        $this->introtitle = $info['profiles_introtitle'];
+        $this->introtext = $info['profiles_introtext'];
+    }
+
+    public function __get($name)
+    {
+        return $this->$name; 
+    }
+
+    public function getProfile($id){
+        return $this->getProfileInfo($id)[0];
     }
 }
