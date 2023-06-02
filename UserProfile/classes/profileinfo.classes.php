385d31202d86b5cb16d39c81da0dcf52d13c022b
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-06-02 15:56:44 -0600

    47. fix: returning result at getting profile info.

--- UserProfile/classes/profileinfo.classes.php
+++ UserProfile/classes/profileinfo.classes.php
@@ -3,23 +3,21 @@
 class ProfileInfo extends Dbh {
 
     protected function getProfileInfo($userId){
-        $sql = "SELECT * FROM profiles WHERE users_id = ?;";
+        $sql = "SELECT profiles_about, profiles_introtitle, profiles_introtext FROM profiles WHERE users_id = ?";
         $stmt = $this->connect()->prepare($sql);
         $result = $stmt->execute([$userId]);
 
         if(!$result){
             $stmt = null;
-            header('Location: profile.php?error=stmtfailed');
-            exit();
+            return false;
         }
+
+        return $stmt->fetchAll();
     }
 
     protected function setNewProfile($profileAbout, $profileTitle, $profileText, $profileId){
         $sql = "INSERT INTO profiles (profiles_about, profiles_introtitle, profiles_introtext, users_id) VALUES (?, ?, ?, ?)";
         $stmt = $this->connect()->prepare($sql);
         $result = $stmt->execute([$profileAbout, $profileTitle, $profileText, $profileId]);
-
-
-        print_r($result);
     }
 }
