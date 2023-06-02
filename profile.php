811a1c743d445199b1dc1db92071ed36c7694459
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-06-02 16:08:49 -0600

    50. feat: instancing view to get profile data.

--- profile.php
+++ profile.php
@@ -1,4 +1,11 @@
 <?php
+
+    include './includes/header.php';
+    include './classes/dbh.classes.php';
+    include './UserProfile/classes/profileinfo.classes.php';
+    include './UserProfile/classes/profileinfo-view.classes.php';
+
+    $profileInfo = new ProfileInfoView($_SESSION['id']);        
 ?>
 
 <!DOCTYPE html>
