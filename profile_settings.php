81dde420b44e8c3d6daa34b1172199756e9a37ee
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-06-03 18:08:44 -0600

    "56. including getProfileData file into profile_settings page""
    This reverts commit ce7504b86b6a820485b357b37d33e6eb89d11846.

--- profile_settings.php
+++ profile_settings.php
@@ -1,4 +1,5 @@
-<?php
+<?php 
+    include './UserProfile/includes/getProfileData.inc.php';    
 ?>
 
 <!DOCTYPE html>
@@ -16,12 +17,12 @@
                 <legend>Profile Settings</legend>
 
                 <small>Change your about section here!</small>
-                <textarea name="about" cols="30" rows="10" placeholder="Profile about section..."></textarea>
+                <textarea name="about" cols="30" rows="10" placeholder="Profile about section..."><?php echo $profileInfo->about; ?></textarea>
 
                 <small>Change your profile page page intro here!</small>
-                <input name="intro" type="text" placeholder="Profile title"></input>
+                <input name="intro" type="text" placeholder="Profile title" value="<?php echo $profileInfo->introtitle; ?>">
 
-                <textarea name="intro" placeholder="Profile introduction..."></textarea>
+                <textarea name="intro" placeholder="Profile introduction..." cols="30" rows="10"><?php echo $profileInfo->introtext; ?> </textarea>
 
                 <input type="submit" value="SAVE" name="inp_save_profile-settings">
             </fieldset>
