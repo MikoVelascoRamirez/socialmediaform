07909a061c84c2674627f541362396605d78c97d
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-06-04 22:58:19 -0600

    59. Instance ProfileInfo controller and calling updated method.

--- UserProfile/includes/profile-settings.inc.php
+++ UserProfile/includes/profile-settings.inc.php
@@ -7,4 +7,11 @@ if(isset($_POST['inp_save_profile-settings'])){
     $titleIntro = htmlspecialchars($_POST['title_intro'], ENT_QUOTES, 'UTF-8');
     $textIntro = htmlspecialchars($_POST['text_intro'], ENT_QUOTES, 'UTF-8');
 
+    include '../../classes/dbh.classes.php';
+    include '../classes/profileinfo.classes.php';
+    include '../classes/profileinfo-contr.classes.php';
+
+    $profileContr = new ProfileInfoContr($_SESSION['id'], $_SESSION['username']);
+    $isUpdated = $profileContr->updateProfileInfo($infoAbout, $titleIntro, $textIntro);
+
 }
