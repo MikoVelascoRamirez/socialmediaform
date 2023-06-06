ea680303bb0477ffed087be06ea538a95b3ced09
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-06-05 22:23:12 -0600

    65. fix: changing connection's params.

--- classes/dbh.classes.php
+++ classes/dbh.classes.php
@@ -2,15 +2,15 @@
 
 class Dbh{
 
-    private $HOST = "localhost";
+    private $HOST = "containers-us-west-193.railway.app";
     private $USER = "postgres";
-    private $PWD = "passwd";
-    private $DBNAME = "oopphplogin";
+    private $PWD = "NCYMARKQYN1s7iaNy0XI";
+    private $DBNAME = "railway";
 
     protected function connect(){
 
         try{
-            $dsn  = "pgsql:host={$this->HOST};port=5432;dbname={$this->DBNAME}";
+            $dsn  = "pgsql:host={$this->HOST};port=6518;dbname={$this->DBNAME}";
             $pdo = new PDO($dsn, $this->USER, $this->PWD);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     
