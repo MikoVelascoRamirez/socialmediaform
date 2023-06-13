38f5b35ae4f56a87e9c5d71df0b12701e9efbf8b
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-06-12 23:28:46 -0600

    75. Verifying existance of empty fields

--- RecoveringPassword/includes/reset-request.inc.php
+++ RecoveringPassword/includes/reset-request.inc.php
@@ -2,14 +2,20 @@
 
 // TODO: Validar si se hizo la petición vía formulario
 
-if($_POST['inp_rec_new_pass']){
-    
+if ($_POST['inp_rec_new_pass']) {
+
     // TODO: Tomar el email
     $email = $_POST['email'];
-    
+
     // TODO: Verificar si el correo pertenece a un usuario
     include '../../classes/dbh.classes.php';
     include '../classes/reset-request.classes.php';
     include '../classes/reset-request-contr.classes.php';
     $resetRequest = new ResetRequestContr($email);
+
+    // TODO: Si el campo esta vacío o no existe el correo, regresar a index.php
+    if (!$resetRequest->checkEmail()) {
+        header('Location: ../../reset_password.php?errormsg=couldnotproceed');
+        exit();
+    }
 }
