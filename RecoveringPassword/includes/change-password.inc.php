024e92db10a471d264d04db95c48acdac0389159
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-06-13 13:10:04 -0600

    98. Instance ChangePasswordController's object.

--- RecoveringPassword/includes/change-password.inc.php
+++ RecoveringPassword/includes/change-password.inc.php
@@ -1,8 +1,8 @@
 <?php
 
 //TODO: Validar que se haya accedido mediante formulario
-if(isset($_POST['reset_pass'])){
-    
+if (isset($_POST['reset_pass'])) {
+
     // TODO: Obtener el token de selector
     $tokenSelector = $_POST['token_selector'];
     // echo $tokenSelector;
@@ -12,4 +12,10 @@ if(isset($_POST['reset_pass'])){
     $queryString = parse_url($previousURL, PHP_URL_QUERY);
     parse_str($queryString, $params);
 
+    // TODO: Instanciar objeto de controlador
+    include_once('../../classes/dbh.classes.php');
+    include_once('../classes/change-password.classes.php');
+    include_once('../classes/change-password-contr.classes.php');
+
+    $recov_request = new ChangePasswordContr();
 }
