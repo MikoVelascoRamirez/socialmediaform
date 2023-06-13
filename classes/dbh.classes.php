e2563dd1b9ef1d12546af32ca212cf02d508069b
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-06-13 13:28:45 -0600

    106. refactor: setting port value into a variable.

--- classes/dbh.classes.php
+++ classes/dbh.classes.php
@@ -1,16 +1,16 @@
 <?php
 
 class Dbh{
-
     private $HOST = "containers-us-west-193.railway.app";
     private $USER = "postgres";
     private $PWD = "NCYMARKQYN1s7iaNy0XI";
     private $DBNAME = "railway";
+    prIvate $PORT = 6518;
 
     protected function connect(){
 
         try{
-            $dsn  = "pgsql:host={$this->HOST};port=6518;dbname={$this->DBNAME}";
+            $dsn  = "pgsql:host={$this->HOST};port={$this->PORT};dbname={$this->DBNAME}";
             $pdo = new PDO($dsn, $this->USER, $this->PWD);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     
