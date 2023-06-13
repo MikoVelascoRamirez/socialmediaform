cb4e3ba094e4b6f1711a7423cee74f28474861fd
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-06-12 23:24:41 -0600

    74. taking email and including files to verify if an e-mail belongs to an user. Instanciating reset request coontroller.

--- RecoveringPassword/includes/reset-request.inc.php
+++ RecoveringPassword/includes/reset-request.inc.php
@@ -2,5 +2,14 @@
 
 // TODO: Validar si se hizo la petición vía formulario
 
-if($_POST['inp_rec_new_pass']){    
+if($_POST['inp_rec_new_pass']){
+    
+    // TODO: Tomar el email
+    $email = $_POST['email'];
+    
+    // TODO: Verificar si el correo pertenece a un usuario
+    include '../../classes/dbh.classes.php';
+    include '../classes/reset-request.classes.php';
+    include '../classes/reset-request-contr.classes.php';
+    $resetRequest = new ResetRequestContr($email);
 }
